d0719defd362abcb006bfa544d0a85c9d9e9d1e8
DanikKul <92942037+DanikKul@users.noreply.github.com>
2022-02-05 14:01:04 +0300

    fix: bug with colors

--- code.js
+++ code.js
@@ -19,7 +19,7 @@ const jackie = document.querySelector('#img-container');
 const text = document.querySelector('.main-container .quote');
 const author = document.querySelector('.main-container .author');
 const myHtml = document.querySelector('html');
-let colors = ['black', 'green', 'blue', 'red', 'purple', 'cornflowerblue', 'violet', 'chocolate', 'dimgray', 'aqua', 'brown', 'darkkhaki'];
+let colors = ['black', 'green', 'blue', 'red', 'purple', 'cornflowerblue', 'violet', 'chocolate', 'dimgray', 'brown', 'darkkhaki'];
 
 function loaded() {
     let x = Math.floor(Math.random() * 100);
@@ -29,9 +29,9 @@ function loaded() {
 
 buttonQuote.addEventListener('click', function (){
     let current = myHtml.style.backgroundColor;
-    let newColor = colors[Math.floor(Math.random() * 13)];
+    let newColor = colors[Math.floor(Math.random() * 11)];
     while(current === newColor){
-        newColor = colors[Math.floor(Math.random() * 13)];
+        newColor = colors[Math.floor(Math.random() * 11)];
     }
     myHtml.style.backgroundColor = newColor;
     jackie.setAttribute('class', 'img-container');
